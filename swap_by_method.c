7d72229ce356bada72fa079fc53775a64c8d5f68
Bryan <baliddeki@gmail.com>
2023-06-25 10:27:51 +0300

    some required comments

--- swap_by_method.c
+++ swap_by_method.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//swap method
 void swap(int num1, int num2){
     int temp;
     temp = num1;
@@ -12,7 +13,11 @@ int main(){
     int a = 5, b = 7;
 
     printf("Numbers before swap \n a = %d, b = %d \n" , a, b);
+
+    //calling swap method
     swap(a, b);
+
+    //a and b values are not swapped since they are passed by value and not by reference
     printf("Numbers after swap \n a = %d, b = %d" , a, b);
     return (0);
 }
