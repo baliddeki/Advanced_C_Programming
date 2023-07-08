0d6cc7a3248cd58565df60adcb4e560b0e497801
Bryan <baliddeki@gmail.com>
2023-07-08 14:39:45 +0300

    swappping two values by pointers

---
+++ swap_by_pointers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int swap(int *num1, int *num2)
+{
+    int temp;
+    temp = *num1;
+    printf("%d\n", *num1);
+    printf("%d\n", *num2);
+    printf("%d\n", temp);
+    printf("--------------------------------------------\n");
+    *num1 = *num2;
+    printf("%d\n", *num1);
+    printf("%d\n", *num2);
+    printf("%d\n", temp);
+    printf("--------------------------------------------\n");
+    *num2 = temp;
+    printf("%d\n", *num1);
+    printf("%d\n", *num2);
+    printf("%d\n", temp);
+    printf("--------------------------------------------\n");
+}
+
+int main()
+{
+    /**
+     * int a = 13, b = 25;
+     * int temp1, temp2;
+     * swap(a, b);
+     * printf("The swapped new value of a is %d and new value of b is %d ", a, b);
+     */
+    int num1 = 13, num2 = 14;
+    swap(&num1, &num2);
+    printf("The swapped new value of a is %d and new value of b is %d ", num1, num2);
+}
